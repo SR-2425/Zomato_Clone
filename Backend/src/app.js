475dcb7cc457d0c9b1abbfafa702f832f5209150
SR-2425 <sanskarraskar56@gmail.com>
2025-03-08 19:55:34 +0530

    Added Frontend URL

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 //Middlewares
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://tomato-gyct.onrender.com',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
