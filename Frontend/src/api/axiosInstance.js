543f261a29e0f226f83dd78cea46a11dd9e1483c
SR-2425 <sanskarraskar56@gmail.com>
2025-03-09 17:14:12 +0530

    Added changes

--- Frontend/src/api/axiosInstance.js
+++ Frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "https://tomato-gyct.onrender.com",  // ✅ Ensure this is correct
+  baseURL: "https://tomato-gyct.onrender.com/api",  // ✅ Ensure this is correct
   withCredentials: true,  // ✅ This applies globally to all requests
   headers: {
     "Content-Type": "application/json"
