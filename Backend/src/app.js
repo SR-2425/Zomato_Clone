c9377e4d1a712356e6115096ff23e9c12925f21c
SR-2425 <sanskarraskar56@gmail.com>
2025-03-09 18:17:31 +0530

    changed frontend url

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 //Middlewares
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['https://tomato-y8pa.onrender.com'],
 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
